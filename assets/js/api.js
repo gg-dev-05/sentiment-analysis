75167c12cdcc1806ab75f5c0c826fa9eeaf7e45f
gg-dev-05 <garvitgalgat@gmail.com>
2022-04-17 07:35:44 +0530

    js for sentiment

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,14 @@
 const URL = "https://g14-analyzer.herokuapp.com";
+const sentimentContainer = document.getElementById("sentiment-container");
+let interval;
 document
   .querySelector(".sentiment-form")
   .addEventListener("submit", async (e) => {
     e.preventDefault();
+    // sentimentContainer.style.display = "block";
+    sentimentContainer.classList.remove("invisiblity");
+    // sentimentContainer.classList.add("not-invisiblity");
+    document.getElementById("sentiment").innerHTML = "<i>Analysing...</i>";
     const formData = new FormData(e.target);
     const sentence = formData.get("sentence");
     if (!sentence) return;
@@ -18,5 +24,18 @@ document
     });
     const data = await response.json();
     const sentiment = data["sentiment"];
-    alert(sentiment);
+    toggleSentiment(sentiment);
+    interval = setTimeout(() => {
+      // sentimentContainer.style.display = "none";
+      sentimentContainer.classList.add("invisiblity");
+      // sentimentContainer.classList.remove("not-invisiblity");
+    }, 5000);
   });
+
+function toggleSentiment(sentiment) {
+  clearTimeout(interval);
+  // sentimentContainer.style.display = "block";
+  sentimentContainer.classList.remove("invisiblity");
+  // sentimentContainer.classList.add("not-invisiblity");
+  document.getElementById("sentiment").innerText = sentiment;
+}
